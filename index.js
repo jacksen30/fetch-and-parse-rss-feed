7f871915c097754409d4847dcc3aca456712881e
Jacksen Nillson <nillsonjacksen@gmail.com>
2024-03-23 16:46:40 +0800

    send request via x-cors, map the returned data to render to UI

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const mainContentSection = document.getElementById('main');
+
 function fetchAndParseRSS(url) {
     // Prepending a CORS proxy to the RSS feed URL
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://proxy.cors.sh/';
     const fullUrl = proxyUrl + url;
 
-    fetch(fullUrl)
+    fetch(fullUrl, {
+        headers: {
+        'x-cors-api-key': 'X-COR-API-KEY_WILL-GO_HERE'
+        }
+    })
       .then(response => response.text())
       .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
       .then(data => {
@@ -12,12 +18,24 @@ function fetchAndParseRSS(url) {
 
         items.forEach(item => {
           parsedItems.push({
-            title: item.querySelector("title").textContent,
-            link: item.querySelector("link").textContent,
-            description: item.querySelector("description").textContent,
-            publicationDate: item.querySelector("pubDate").textContent,
+            title: item.querySelector("trading-name").textContent,
+            price: item.querySelector("price").textContent,
+            address: item.querySelector("address").textContent,
+            suburb: item.querySelector("location").textContent,
           });
         });
+
+        const renderDivsHtml = parsedItems.map(servo => {
+            return `<div class="fuel-location-wrapper">
+                <p>${servo.title}</p>
+                <p>${servo.price}</p>
+                <p>${servo.address}</p>
+                <p>${servo.suburb}</p>
+            </div>`
+        });
+
+        mainContentSection.innerHTML = renderDivsHtml.join(' ');
+        console.log(renderDivsHtml);
 
         // Now, parsedItems contains the RSS feed data in a JSON-like format
         console.log(parsedItems);
@@ -26,4 +44,4 @@ function fetchAndParseRSS(url) {
   }
 
   // Example usage
-  fetchAndParseRSS('http://example.com/feed.xml');
+  fetchAndParseRSS('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Geraldton');
